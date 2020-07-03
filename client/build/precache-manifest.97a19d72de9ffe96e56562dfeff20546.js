915e21ea692046fbcb5b33bb33b42cfd7832ceaf
Meris Tarhanis <gzgz.mk@gmail.com>
2020-07-03 05:46:45 +0200

    try n3

---
+++ client/build/precache-manifest.97a19d72de9ffe96e56562dfeff20546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffdb90735dca9488f43088747eb9351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d279562276ab750761",
+    "url": "/static/css/main.253372c2.chunk.css"
+  },
+  {
+    "revision": "ca3c68f80f2ca18d92c2",
+    "url": "/static/js/2.b07603ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b07603ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d279562276ab750761",
+    "url": "/static/js/main.0383fb39.chunk.js"
+  },
+  {
+    "revision": "5f7ebdf64cfaafba7f1d",
+    "url": "/static/js/runtime-main.22a09854.js"
+  }
+]);
